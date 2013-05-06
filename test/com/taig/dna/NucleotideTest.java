97ad41448c1c15ad4de138aa36a767382fdfff57
Taig <taig@AiR>
2013-05-06 20:08:18 +0200

    Added unit tests.

---
+++ test/com/taig/dna/NucleotideTest.java
@@ -0,0 +1,23 @@
+package com.taig.dna;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class NucleotideTest
+{
+	@Test
+	public void newInstanceWithValidAbbreviations()
+	{
+		assertTrue( Nucleotide.newInstance( 'A' ) instanceof Nucleotide.Purine.Adenine );
+		assertTrue( Nucleotide.newInstance( 'C' ) instanceof Nucleotide.Pyrimidine.Cytosine );
+		assertTrue( Nucleotide.newInstance( 'G' ) instanceof Nucleotide.Purine.Guanine );
+		assertTrue( Nucleotide.newInstance( 'T' ) instanceof Nucleotide.Pyrimidine.Thymine );
+	}
+
+	@Test( expected = IllegalArgumentException.class )
+	public void newInstanceWithInvalidAbbreviation()
+	{
+		Nucleotide.newInstance( 'X' );
+	}
+}
---
+++ test/com/taig/dna/SequenceTest.java
@@ -0,0 +1,40 @@
+package com.taig.dna;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SequenceTest
+{
+	@Test( expected = NullPointerException.class )
+	public void constructSequenceWithNullStringArgument()
+	{
+		new Sequence( (String) null );
+	}
+
+	@Test( expected = IllegalArgumentException.class )
+	public void constructSequenceWithInvalidStringResource()
+	{
+		new Sequence( "acxgt" );
+	}
+
+	@Test
+	public void constructSequenceWithValidStringResource()
+	{
+		Sequence sequence = new Sequence( "acgt" );
+
+		assertEquals( 4, sequence.size() );
+		assertTrue( sequence.get( 0 ) instanceof Nucleotide.Purine.Adenine );
+		assertTrue( sequence.get( 1 ) instanceof Nucleotide.Pyrimidine.Cytosine );
+		assertTrue( sequence.get( 2 ) instanceof Nucleotide.Purine.Guanine );
+		assertTrue( sequence.get( 3 ) instanceof Nucleotide.Pyrimidine.Thymine );
+	}
+
+	@Test
+	public void toStringWithHealthySequence()
+	{
+		String sequence = "acgt";
+
+		assertEquals( new Sequence( sequence ).toString(), sequence.toUpperCase() );
+	}
+}
