5638e74ee6e4188e754de23f22f88c370dd9c6cc
Taig <taig@AiR>
2013-05-08 02:58:12 +0200

    Prepared unit tests for class CodingContest.

---
+++ test/com/taig/dna/CodingContestTest.java
@@ -0,0 +1,96 @@
+package com.taig.dna;
+
+import org.junit.Test;
+
+public class CodingContestTest
+{
+	@Test
+	public void hasRiskOfTiberiusSyndromeOnUnaffectedSequence()
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	@Test
+	public void hasRiskOfTiberiusSyndromeOnAffectedSequence()
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	@Test
+	public void hasBrownEyesOnUnaffectedSequence()
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	@Test
+	public void hasBrownEyesOnAffectedSequence()
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	@Test
+	public void countNucleotidesOnEmptySequence()
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	@Test
+	public void countNucleotidesOnHealtySequence()
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	@Test
+	public void findFirstCtagOccurrenceOnEmptySequence()
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	@Test
+	public void findFirstCtagOccurrenceOnHealthySequenceWithoutCtag()
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	@Test
+	public void findFirstCtagOccurrenceOnHealthySequenceWithCtag()
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	@Test
+	public void hasMorePurinesThanPyrimidinesOnEmptySequence()
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	@Test
+	public void hasMorePurinesThanPyrimidinesOnHealthySequence()
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	@Test
+	public void hasFromingenDischrypsiaEvidenceOnUnaffectedSequence()
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	@Test
+	public void hasFromingenDischrypsiaEvidenceOnAffectedSequence()
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	@Test
+	public void getComplementSequenceOnEmptySequence()
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	@Test
+	public void getComplementSequenceOnHealthySequence()
+	{
+		throw new UnsupportedOperationException();
+	}
+}
